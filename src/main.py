a9ec4e78d78045c183ab61f5ae1c6de2ac30965f
Alexander Zatuliveter <alexander.zatuliveter@gmail.com>
2025-05-05 17:07:17 +0600

    .gitignore, consts and start player added.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ import pygame
 import ctypes
 from pygame.locals import DOUBLEBUF, OPENGL, RESIZABLE
 from OpenGL.GL import *  # type: ignore
-from OpenGL.GLU import *  # type: ignore
+from OpenGL.GLU import *
+
+from consts import GAME_FIELD_HEIGHT, GAME_FIELD_WIDTH
+from player import Player  # type: ignore
 
 
 # Set process DPI awareness. Use 1 for "System DPI Awareness", or 2 for "Per-Monitor DPI Awareness"
@@ -19,13 +22,24 @@ screen_size = info.current_w * 0.7, info.current_h * 0.7
 screen = pygame.display.set_mode(screen_size, DOUBLEBUF | OPENGL | RESIZABLE)
 pygame.display.set_caption("ShapeSpaceGame")
 
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluOrtho2D(0, screen_size[0], screen_size[1], 0)  # (left, right, bottom, top)
+glMatrixMode(GL_MODELVIEW)
+glLoadIdentity()
+
 clock = pygame.time.Clock()
 
+player = Player()
+
+
 while True:
-    glClearColor(150 / 255, 150 / 255, 150 / 255, 1)  # Устанавливаем цвет фона
-    glClear(GL_COLOR_BUFFER_BIT)  # Очищаем экран
+    glClearColor(100 / 255, 100 / 255, 100 / 255, 1)  # set background's color
+    glClear(GL_COLOR_BUFFER_BIT)  # clear screen
 
     events = pygame.event.get()
+    keys = pygame.key.get_pressed()
+
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -34,9 +48,35 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+        if event.type == pygame.VIDEORESIZE:
+            screen_size = event.w, event.h
+            glViewport(0, 0, event.w, event.h)
+            glMatrixMode(GL_PROJECTION)
+            glLoadIdentity()
+            gluOrtho2D(0, event.w, event.h, 0)
+            glMatrixMode(GL_MODELVIEW)
+            glLoadIdentity()
 
-    # Update the display
+    player.update(keys)
+
+    # OpenGL drawing.
+
+    glBegin(GL_LINE_LOOP)
+
+    width = GAME_FIELD_WIDTH
+    height = GAME_FIELD_HEIGHT
+    glColor3f(1, 1, 1)
+    glVertex2f(0, 0)
+    glVertex2f(0 + width, 0)
+    glVertex2f(0 + width, 0 + height)
+    glVertex2f(0, 0 + height)
+
+    glEnd()
+
+    glBegin(GL_QUADS)
+    player.draw()
+    glEnd()
+
     pygame.display.flip()
 
-    # Cap the frame rate
     clock.tick(60)
---
+++ src/player.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.key import ScancodeWrapper
+from OpenGL.GL import *  # type: ignore
+
+from consts import GAME_FIELD_HEIGHT, GAME_FIELD_WIDTH, PLAYER_SPEED
+
+
+class Player:
+    def __init__(self):
+        self.__rect = pygame.Rect(GAME_FIELD_WIDTH // 2, GAME_FIELD_HEIGHT // 2, 100, 100)
+
+    def update(self, keys: ScancodeWrapper) -> None:
+        if keys[pygame.K_LEFT] and self.__rect.topleft[0] > 0:
+            self.__rect.x -= PLAYER_SPEED
+        if keys[pygame.K_RIGHT] and self.__rect.bottomright[0] < GAME_FIELD_WIDTH:
+            self.__rect.x += PLAYER_SPEED
+        if keys[pygame.K_UP] and self.__rect.topleft[1] > 0:
+            self.__rect.y -= PLAYER_SPEED
+        if keys[pygame.K_DOWN] and self.__rect.bottomright[1] < GAME_FIELD_HEIGHT:
+            self.__rect.y += PLAYER_SPEED
+
+    def draw(self) -> None:
+        x, y, width, height = self.__rect.x, self.__rect.y, self.__rect.w, self.__rect.h
+        glColor3f(0, 0, 1)
+        glVertex2f(x, y)
+        glVertex2f(x + width, y)
+        glVertex2f(x + width, y + height)
+        glVertex2f(x, y + height)
